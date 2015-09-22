fb8a15321ad8f8912cce645cdf09e25f734997eb
Shawn <rich.shawn@gmail.com>
2015-09-22 17:29:03 -0400

    more examples

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -5,10 +5,16 @@ for (i in 1 : 1000) mean_distro = c(mean_distro, mean(rexp(40,0.2)))
 
 g <- ggplot(data.frame(mean_distro = mean_distro), aes(mean_distro))
 g <- g + geom_histogram(color = "black", fill = "lightblue", binwidth = 0.05)
+g
 g <- g + geom_density(aes(y = ..scaled..))
 g <- g + stat_function(fun = dnorm, size = 2)
 g
 
+mu <- 5
+se <- 5/sqrt(40)
+
+cfunc <- function(x n) sqrt(n) * (x - mu) / se
+std_mean_distro <- sapply(mean_distro, cfunc, 40)
 library(ggplot2)
 library(datasets)
 
@@ -22,3 +28,5 @@ gg <- gg + stat_function(fun=dnorm,
                                   sd=sd(mtcars$mpg)))
 
 gg
+
+
