67bb377b86d585e516fc5b47cc6d406960440068
Shawn <rich.shawn@gmail.com>
2015-09-25 17:04:12 -0400

    Project switched to ggplot2

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -30,6 +30,7 @@ gg <- gg + stat_function(fun=dnorm,
 gg
 
 
+# ======================================================================
 nosim <- 1000
 cfunc <- function(x, n) sqrt(n) * (mean(x) - 3.5) / 1.71
 dat <- data.frame(
@@ -38,7 +39,43 @@ dat <- data.frame(
   ),
   size = factor(rep("40", nosim)))
 
-g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
+g <- ggplot(dat, aes(x = x, fill = size))
+g <- g + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
 g <- g + stat_function(fun = dnorm, size = 2)
 g + facet_grid(. ~ size)
 
+
+# ======================================================================
+n=10000
+means <- cumsum(sample(0:1, n, replace = TRUE))/(1:n)
+g <- ggplot(data.frame(x = 1:n, y = means), aes(x = x, y = y))
+g <- g + geom_hline(yintercept = 0.5) + geom_line(size = 2)
+g <- g + labs(x = "Number of obs", y = "Cumulative mean")
+g
+
+
+# ======================================================================
+#hist(rexp(1000,0.2), col="lightgreen")
+
+dat <- data.frame(x = rexp(1000,0.2), size = factor(rep("40", 1000)))
+
+g <- ggplot(dat, aes(x = x, fill = size))
+g <- g + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
+g
+
+
+# ======================================================================
+dat <- data.frame(Exponential_Sample_Data = rexp(1000,0.2))
+ggplot(dat, aes(x=Exponential_Sample_Data)) + geom_histogram(binwidth=.5, colour="black", fill="lightgreen")
+
+
+# ======================================================================
+nosim <- 1000
+cfunc <- function(x, n) sqrt(n) * (x - 5) / 5
+dat <- data.frame(
+  x = sapply(mean_distro, cfunc, 40),
+  size = factor(rep("40", nosim)))
+
+g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
+g <- g + stat_function(fun = dnorm, size = 2)
+g + facet_grid(. ~ size)
