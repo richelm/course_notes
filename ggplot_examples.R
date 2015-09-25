05faadae60745125b6cd5f900bed98e4005d68b3
Shawn <rich.shawn@gmail.com>
2015-09-25 09:54:45 -0400

    Project updates

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -30,3 +30,15 @@ gg <- gg + stat_function(fun=dnorm,
 gg
 
 
+nosim <- 1000
+cfunc <- function(x, n) sqrt(n) * (mean(x) - 3.5) / 1.71
+dat <- data.frame(
+  x = c(apply(matrix(sample(1 : 6, nosim * 40, replace = TRUE), 
+                     nosim), 1, cfunc, 40)
+  ),
+  size = factor(rep("40", nosim)))
+
+g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
+g <- g + stat_function(fun = dnorm, size = 2)
+g + facet_grid(. ~ size)
+
