6208d7ede2c055a57ed6e2454285f5ecc8ddf560
Shawn Rich <rich.shawn@gmail.com>
2016-05-08 20:31:07 -0400

    project nearly complete

--- machine_learning/project/test_scriptlets.R
+++ machine_learning/project/test_scriptlets.R
@@ -8,3 +8,12 @@ d
 
 d <- d[,-c(1,2)]
 d
+m
+
+r1 <- c(1,2,3)
+r2 <- c(2,3,4)
+r3 <- c(3,4,5)
+ms <- as.data.frame(rbind(r1,r2,r3))
+colnames(ms) <- c("col1","col2","col3")
+row.names(ms) <- c("row1","row2","row3")
+ms
